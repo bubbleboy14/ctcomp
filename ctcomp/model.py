4b777d39028961810a8dc8a36332dd1e92b0629c
bubbleboy14 <mario.balibrera@gmail.com>
2018-06-23 18:09:07 -0700

    model: Content (w/ owner key and identifier [such as url] string); View strings are now keys (CTUser, Content); CTUser import

--- ctcomp/model.py
+++ ctcomp/model.py
@@ -1,5 +1,10 @@
 from cantools import db
+from ctuser.model import CTUser
+
+class Content(db.TimeStampedBase):
+	owner = db.ForeignKey(kind=CTUser)
+	identifier = db.String() # some hash, defaulting to url
 
 class View(db.TimeStampedBase):
-	user = db.String()    # default to ip
-	content = db.String() # content key string of some sort -- url for example
+	viewer = db.ForeignKey(kind=CTUser)
+	content = db.ForeignKey(kind=Content)
