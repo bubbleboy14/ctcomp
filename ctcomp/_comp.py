552a058926b8c3403c0e5c1aff63789d2da7e242
bubbleboy14 <mario.balibrera@gmail.com>
2018-06-23 18:12:46 -0700

    /_comp request handler: key-based user/content, including creating an ip-blurbed user if necessary

--- ctcomp/_comp.py
+++ ctcomp/_comp.py
@@ -1,18 +1,31 @@
 from cantools.web import respond, succeed, fail, cgi_get, local
-from model import View
+from model import db, CTUser, Content, View
 
 def response():
 	action = cgi_get("action", choices=["view"])
 	if action == "view":
-		user = cgi_get("user", required=False) or local("response").ip
-		content = cgi_get("content")
-		if View.query(View.user == user, View.content == content).get():
+		ip = local("response").ip
+		content = db.get(cgi_get("content")) # key
+
+		user = cgi_get("user", required=False) # key
+		if user:
+			user = db.get(user)
+		else:
+			user = CTUser.query(CTUser.blurb == ip).get() # haha
+		if not user:
+			user = CTUser()
+			user.blurb = ip # haha again
+			user.put()
+
+		if View.query(View.viewer == user.key, View.content == content.key).get():
 			succeed("already viewed")
 		view = View()
-		view.user = user
-		view.content = content
+		view.viewer = user.key
+		view.content = content.key
 		view.put()
-		# TODO: create a token!! put it in the right account!
+
+		# TODO: create a token!! put it in the right account! <---
+
 		succeed(view.key.urlsafe())
 	else:
 		fail("what? bad action: %s"%(action,))
