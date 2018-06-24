8a6c48e8bae30525413923bd105c50e921a49dba
bubbleboy14 <mario.balibrera@gmail.com>
2018-06-23 22:29:01 -0700

    model: Wallet (w/ placeholder deposit()); Person (w/ ip and wallet); updated Content.owner and View.viewer (CTUser->Person)

--- ctcomp/model.py
+++ ctcomp/model.py
@@ -1,10 +1,21 @@
 from cantools import db
 from ctuser.model import CTUser
 
+class Wallet(db.TimeStampedBase):
+	identifier = db.String() # for now or whatever
+
+	def deposit(self, amount):
+		# TODO: create a token!! put it in the right account! <---
+		pass
+
+class Person(CTUser):
+	ip = db.String()                    # optional
+	wallet = db.ForeignKey(kind=Wallet) # optional
+
 class Content(db.TimeStampedBase):
-	owner = db.ForeignKey(kind=CTUser)
+	owner = db.ForeignKey(kind=Person)
 	identifier = db.String() # some hash, defaulting to url
 
 class View(db.TimeStampedBase):
-	viewer = db.ForeignKey(kind=CTUser)
+	viewer = db.ForeignKey(kind=Person)
 	content = db.ForeignKey(kind=Content)
