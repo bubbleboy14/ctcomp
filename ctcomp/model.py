da5aa31f704b90b365c95d7cc39232eb980c02c7
bubbleboy14 <mario.balibrera@gmail.com>
2019-04-14 13:38:22 -0700

    model: Service w/ name and compensation; Pod.service()

--- ctcomp/model.py
+++ ctcomp/model.py
@@ -16,12 +16,15 @@ class Pod(db.TimeStampedBase):
 	name = db.String()
 	pool = db.ForeignKey(kind=Wallet)
 
+	def members(self):
+		return [mem.person for mem in Membership.query(Membership.pod == self.key).fetch()]
+
 	def deposit(self, member, amount):
 		member.wallet.get().deposit(amount)
 		self.pool.get().deposit(amount)
 
-	def members(self):
-		return [mem.person for mem in Membership.query(Membership.pod == self.key).fetch()]
+	def service(self, member, service, recipient_count):
+		self.deposit(member, service.compensation * recipient_count)
 
 class Membership(db.TimeStampedBase):
 	pod = db.ForeignKey(kind=Pod)
@@ -34,3 +37,7 @@ class Content(db.TimeStampedBase):
 class View(db.TimeStampedBase):
 	viewer = db.ForeignKey(kind=Person)
 	content = db.ForeignKey(kind=Content)
+
+class Service(db.TimeStampedBase):
+	name = db.String()
+	compensation = db.Float(default=1.0)
