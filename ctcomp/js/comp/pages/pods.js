160091ff96a50a4177cb7919d367f47d476a99db
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-13 20:31:52 -0700

    pods page loader: comp.pods require; call comp.pods.init()

--- ctcomp/js/comp/pages/pods.js
+++ ctcomp/js/comp/pages/pods.js
@@ -2,11 +2,10 @@ CT.require("CT.all");
 CT.require("core");
 CT.require("user.core");
 CT.require("comp.core");
+CT.require("comp.pods");
 CT.require("decide");
 
 CT.onload(function() {
 	CT.initCore();
-	CT.db.get("membership", comp.core.pods, null, null, null, {
-		person: user.core.get("key")
-	});
+	comp.pods.init();
 });
