a378cdc782293a1dde007054156a4e53ab4f4fdb
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-14 17:40:10 -0700

    /_comp handler: support commitments

--- ctcomp/_comp.py
+++ ctcomp/_comp.py
@@ -1,8 +1,8 @@
 from cantools.web import respond, succeed, fail, cgi_get, local
-from model import db, Person, Content, View, Act, Request
+from model import db, Person, Content, View, Act, Commitment, Request
 
 def response():
-	action = cgi_get("action", choices=["view", "act", "verify", "request", "proposals"])
+	action = cgi_get("action", choices=["view", "act", "commit", "request", "verify", "proposals"])
 	if action == "view":
 		ip = local("response").ip
 		content = db.get(cgi_get("content")) # key
@@ -30,16 +30,21 @@ def response():
 		succeed(view.key.urlsafe())
 	elif action == "act":
 		act = Act()
+		act.membership = cgi_get("membership")
 		act.service = cgi_get("service")
-		act.pod = cgi_get("pod")
 		act.workers = cgi_get("workers")
 		act.beneficiaries = cgi_get("beneficiaries")
 		act.notes = cgi_get("notes")
 		act.put()
 		succeed(act.key.urlsafe())
-	elif action == "verify":
-		act = db.get(cgi_get("act")) # act or request
-		act.verify(cgi_get("person"))
+	elif action == "commit":
+		comm = Commitment()
+		comm.membership = cgi_get("membership")
+		comm.service = cgi_get("service")
+		comm.estimate = cgi_get("estimate")
+		comm.put()
+		comm.verify(comm.membership.get().person) # (submitter already agrees)
+		succeed(comm.key.urlsafe())
 	elif action == "request":
 		req = Request()
 		req.action = cgi_get("action")
@@ -48,9 +53,10 @@ def response():
 		req.notes = cgi_get("notes")
 		req.put()
 		succeed(req.key.urlsafe())
+	elif action == "verify":
+		verifiable = db.get(cgi_get("verifiable")) # act or request or commitment
+		verifiable.verify(cgi_get("person"))
 	elif action == "proposals":
 		succeed(db.get(cgi_get("pod")).proposals())
-	else:
-		fail("what? bad action: %s"%(action,))
 
 respond(response)
