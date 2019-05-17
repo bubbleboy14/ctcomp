c2dc8a6b9ed2642dd013025c6ff40b2dc7ee2830
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-16 19:43:17 -0700

    email templates: use config.web.protocol; EXCLUDE template

--- ctcomp/compTemplates.py
+++ ctcomp/compTemplates.py
@@ -4,7 +4,7 @@ APPLY = """Hello!
 
 %s has invited you to join a pod called "%s".
 
-Click <a href='http://""" + config.web.domain + """/_comp?action=apply&request=%s'>here</a> to apply.
+Click <a href='""" + config.web.protocol + """://""" + config.web.domain + """/_comp?action=apply&request=%s'>here</a> to apply.
 
 That's it!"""
 
@@ -12,6 +12,14 @@ APPLICATION = """Hello!
 
 %s just applied for membership in the "%s" pod.
 
-Please click <a href='http://""" + config.web.domain + """/_comp?action=verify&verifiable=%s&person=%s'>here</a> if you wish to allow it.
+Please click <a href='""" + config.web.protocol + """://""" + config.web.domain + """/_comp?action=verify&verifiable=%s&person=%s'>here</a> if you wish to allow it.
+
+That's it!"""
+
+EXCLUDE = """Hello!
+
+%s has proposed excluding %s from the "%s" pod.
+
+Please click <a href='""" + config.web.protocol + """://""" + config.web.domain + """/_comp?action=verify&verifiable=%s&person=%s'>here</a> if you agree that this person should be excluded.
 
 That's it!"""
