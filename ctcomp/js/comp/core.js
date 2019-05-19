a589b800c7b09a3ceb4109e749a188a40f02ae9e
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-18 18:21:55 -0700

    core.edit()

--- ctcomp/js/comp/core.js
+++ ctcomp/js/comp/core.js
@@ -6,6 +6,17 @@ comp.core = {
 			cb: cb
 		});
 	},
+	edit: function(data, cb) {
+		CT.net.post({
+			path: "/_db",
+			params: {
+				data: data,
+				action: "edit",
+				pw: core.config.keys.storage
+			},
+			cb: cb
+		});
+	},
 	proposals: function(pod, cb) {
 		comp.core.c({
 			action: "proposals",
