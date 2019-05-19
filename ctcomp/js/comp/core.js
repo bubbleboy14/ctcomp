ed9407e26d655b6d57412aa58ea7aabd485e97c0
bubbleboy14 <mario.balibrera@gmail.com>
2019-05-18 23:06:17 -0700

    comp.core: _.service_keys{}; pod(), prompt(), choice(), services(), service(), init()

--- ctcomp/js/comp/core.js
+++ ctcomp/js/comp/core.js
@@ -1,4 +1,7 @@
 comp.core = {
+	_: {
+		service_keys: {}
+	},
 	c: function(opts, cb) {
 		CT.net.post({
 			path: "/_comp",
@@ -17,10 +20,54 @@ comp.core = {
 			cb: cb
 		});
 	},
-	proposals: function(pod, cb) {
+	pod: function(pod, cb) {
 		comp.core.c({
-			action: "proposals",
+			action: "pod",
 			pod: pod
 		}, cb);
+	},
+	prompt: function(opts) {
+		(new CT.modal.Prompt(CT.merge(opts, {
+			transition: "slide"
+		}))).show();
+	},
+	choice: function(opts) {
+		comp.core.prompt(CT.merge(opts, {
+			noClose: true,
+			defaultIndex: 0,
+			style: "single-choice"
+		}));
+	},
+	services: function(cb) {
+		comp.core.choice({
+			prompt: "select a service",
+			data: ["New Service"].concat(comp.core._.services),
+			cb: function(service) {
+				if (service == "New Service") {
+					comp.core.prompt({
+						prompt: "what's the service called?"
+						cb: function(sname) {
+							comp.core.edit({
+								modelName: "service",
+								name: sname
+							}, cb);
+						}
+					});
+				} else
+					cb(service);
+			}
+		});
+	},
+	service: function(skey) {
+		return comp.core._.service_keys[skey];
+	},
+	init: function() {
+		var _ = comp.core._;
+		CT.db.get("service", function(services) {
+			_.services = services;
+			services.forEach(function(service) {
+				_.service_keys[service.key] = service;
+			});
+		});
 	}
 };
