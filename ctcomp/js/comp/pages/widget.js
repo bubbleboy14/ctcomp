545270a7f380fdbb120b2e274e832f812195b664
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-01 16:18:28 -0700

    widget page loader: rmed most imports (data, db, modal, parse, storage, trans, user.core)

--- ctcomp/js/comp/pages/widget.js
+++ ctcomp/js/comp/pages/widget.js
@@ -1,12 +1,5 @@
-CT.require("CT.data");
-CT.require("CT.db");
 CT.require("CT.dom");
-CT.require("CT.modal");
-CT.require("CT.parse");
-CT.require("CT.storage");
-CT.require("CT.trans");
 CT.require("core");
-CT.require("user.core");
 CT.require("comp.core");
 CT.require("comp.widget");
 
