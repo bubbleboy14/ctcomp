6b036f5a9741b18c766b7c287a96ac81f7500f20
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-01 21:35:17 -0700

    widget: switcheroo (switch/enroll) support

--- ctcomp/js/comp/widget.js
+++ ctcomp/js/comp/widget.js
@@ -1,15 +1,47 @@
 comp.widget = {
 	_: {
 		receive: function(event) {
-			var d = event.data;
+			var d = event.data, data = d.data, _ = comp.widget._;
+			_.targetOrigin = event.origin;
 			if (d.action == "view") {
-				comp.core.c(CT.merge(d.data, {
+				comp.core.c(CT.merge(data, {
 					action: "view"
 				}));
+			} else if (d.action == "enroll") {
+				comp.core.c({
+					action: "enroll",
+					agent: data.agent,
+					pod: data.pod,
+					person: user.core.get("key")
+				}, function(memkey) {
+					_.done(memkey, "enrollment");
+				});
 			}
+		},
+		done: function(data, action) {
+			var _ = comp.widget._, d = {
+				action: action,
+				data: data
+			};
+			window.parent.postMessage(d, _.targetOrigin);
+		},
+		setSwitcher: function() {
+			var bignode = CT.dom.div(null, "biggerest bigpadded down30"), upyou = function() {
+				var u = user.core.get();
+				CT.dom.setContent(bignode, u ? ("you are " + u.email) : "Who Are You?");
+				comp.widget._.done(u && u.email, "switch");
+			};
+			CT.dom.setContent("ctmain", CT.dom.div([
+				user.core.links(null, true),
+				bignode
+			], "h1 wm400p automarg centered"));
+			user.core.onchange(upyou);
+			setTimeout(upyou, 1000);
 		}
 	},
 	init: function() {
 		window.addEventListener("message", comp.widget._.receive);
+		if (document.location.hash.slice(1) == "switcheroo")
+			comp.widget._.setSwitcher();
 	}
 };
