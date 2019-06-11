c2301b55e3c5b339763b8d91f24986ad8959dc29
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-10 20:46:38 -0700

    /_comp request handler: on conversation Request, notify() signers

--- ctcomp/_comp.py
+++ ctcomp/_comp.py
@@ -1,6 +1,6 @@
 from cantools.web import respond, succeed, fail, cgi_get, log, local, send_mail, redirect
 from model import db, enroll, manage, Person, Content, View, Act, Commitment, Request, Expense
-from compTemplates import APPLY, APPLICATION, EXCLUDE, SERVICE, COMMITMENT, EXPENSE, CONFCODE
+from compTemplates import APPLY, APPLICATION, EXCLUDE, SERVICE, COMMITMENT, EXPENSE, CONFCODE, CONVO
 from cantools import config
 
 def view(user, content):
@@ -87,9 +87,12 @@ def response():
 		if req.change == "include":
 			send_mail(to=rpmail, subject="pod membership nomination",
 				body=APPLY%(mpmail, pod.name, rkey))
-		else: # exclude
+		elif req.change == "exclude":
 			req.notify("pod membership exclusion proposal",
 				lambda signer : EXCLUDE%(mpmail, rpmail, pod.name, rkey, signer.urlsafe()))
+		else: # conversation
+			req.notify("pod conversation request",
+				lambda signer : CONVO%(mpmail, pod.name, req.notes, rkey, signer.urlsafe()))
 		succeed(req.key.urlsafe())
 	elif action == "expense":
 		exp = Expense()
