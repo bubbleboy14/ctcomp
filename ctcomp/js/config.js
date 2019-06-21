3198561078d23eea3bc521b1f55709e5b108cf55
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-20 18:35:25 -0700

    config, w/ limits{}, classes{}, blurbs{}

---
+++ ctcomp/js/config.js
@@ -0,0 +1,19 @@
+{
+	"limits": {
+		"services": 10,
+		"commitments": 40
+	},
+	"classes": {
+		"menu": "margined padded bordered round"
+	},
+	"blurbs": {
+		"commitment": "Register a weekly commitment.",
+		"service": "Record a one-off service.",
+		"request": "Include and exclude pod members. Schedule conversations.",
+		"content": "Submit web content associated with this pod (most managed pods don't require manual registration).",
+		"codebase": "Register the codebases associated with this software pod, including platform and r&d repositories.",
+		"dependency": "Please select the frameworks used by this project.",
+		"expense": "Propose an expense (currently supported: one-off dividend).",
+		"info": "Here's some basic info about this pod."
+	}
+}
