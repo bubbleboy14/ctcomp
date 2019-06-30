92bb50ea2dee4b2355bc7e9434db6d1e4647cc8b
bubbleboy14 <mario.balibrera@gmail.com>
2019-06-29 18:05:25 -0700

    Pod.blurb

--- ctcomp/model.py
+++ ctcomp/model.py
@@ -63,6 +63,7 @@ class Person(Member):
 class Pod(db.TimeStampedBase):
 	name = db.String()
 	variety = db.String()
+	blurb = db.Text()
 	pool = db.ForeignKey(kind=Wallet)
 	agent = db.ForeignKey(kind="Pod")
 	dependencies = db.ForeignKey(kind="Codebase", repeated=True) # software pod only
