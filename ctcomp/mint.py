2454dd2e27da620d6888d7700251d7f46f29e1cf
bubbleboy14 <mario.balibrera@gmail.com>
2019-08-07 16:24:18 -0700

    mint: basic working module (TODO: Classify)

--- ctcomp/mint.py
+++ ctcomp/mint.py
@@ -1,6 +1,25 @@
 from web3.auto import w3
+from cantools.util import read
+from cantools import config
+cfg = config.contract
+
+CON = None
+
+def contract():
+	global CON
+	if CON:
+		return CON
+	if not (cfg.abi and cfg.owner and cfg.address):
+		return None
+	w3.eth.defaultAccount = cfg.owner
+	CON = w3.eth.contract(read(cfg.abi)).at(cfg.address)
+	return CON
 
 def mint(account, amount):
 	if not (account and amount and w3.isConnected()):
 		return False
-	# TODO: mint amount here, return True on success
+	con = contract()
+	if not con:
+		return False
+	con.mint(account, amount)
+	return True
