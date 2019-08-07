b60da33c98a85ba739f6e37de79d5a6ffae12d4d
bubbleboy14 <mario.balibrera@gmail.com>
2019-08-07 16:29:41 -0700

    mint Classification!!!!

--- ctcomp/mint.py
+++ ctcomp/mint.py
@@ -1,25 +1,19 @@
 from web3.auto import w3
 from cantools.util import read
 from cantools import config
+
+class Mint(object):
+	def __init__(self, abi, owner, address):
+		if abi and owner and address and w3.isConnected():
+			w3.eth.defaultAccount = owner
+			self.contract = w3.eth.contract(read(abi)).at(address)
+
+	def mint(self, account, amount):
+		if account and amount and self.contract and w3.isConnected():
+			self.contract.mint(account, amount)
+			return True
+		return False
+
 cfg = config.contract
-
-CON = None
-
-def contract():
-	global CON
-	if CON:
-		return CON
-	if not (cfg.abi and cfg.owner and cfg.address):
-		return None
-	w3.eth.defaultAccount = cfg.owner
-	CON = w3.eth.contract(read(cfg.abi)).at(cfg.address)
-	return CON
-
-def mint(account, amount):
-	if not (account and amount and w3.isConnected()):
-		return False
-	con = contract()
-	if not con:
-		return False
-	con.mint(account, amount)
-	return True
+minter = mint(cfg.abi, cfg.owner, cfg.address)
+mint = minter.mint
