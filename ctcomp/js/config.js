c06397cf57c18848b54e3da9df57dcdc9fe2c715
bubbleboy14 <mario.balibrera@gmail.com>
2019-10-27 16:02:21 -0700

    config.blurbs{}: capitalized and pluralized keys; added Proposals and Updates

--- ctcomp/js/config.js
+++ ctcomp/js/config.js
@@ -7,15 +7,17 @@
 		"menu": "margined padded bordered round"
 	},
 	"blurbs": {
-		"responsibility": "Task management, including scheduling of and volunteering for pod-related activities.",
-		"commitment": "Register a weekly commitment.",
-		"service": "Record a one-off service.",
-		"request": "Include and exclude pod members. Schedule conversations. Update your pod's blurb.",
-		"content": "Submit web content associated with this pod (most managed pods don't require manual registration).",
-		"codebase": "Register the codebases associated with this software pod, including platform and r&d repositories.",
-		"dependency": "Please select the frameworks used by this project.",
-		"expense": "Propose an expense (currently supported: one-off dividend).",
-		"info": "Here's some basic info about this pod.",
-		"product": "Sell goods and services. Ten percent goes to your pod."
+		"Responsibilities": "Task management, including scheduling of and volunteering for pod-related activities.",
+		"Commitments": "Register a weekly commitment.",
+		"Services": "Record a one-off service.",
+		"Requests": "Include and exclude pod members. Schedule conversations. Update your pod's blurb.",
+		"Content": "Submit web content associated with this pod (most managed pods don't require manual registration).",
+		"Codebases": "Register the codebases associated with this software pod, including platform and r&d repositories.",
+		"Dependencies": "Please select the frameworks used by this project.",
+		"Expenses": "Propose an expense (currently supported: one-off dividend).",
+		"Info": "Here's some basic info about this pod.",
+		"Products": "Sell goods and services. Ten percent goes to your pod.",
+		"Proposals": "Discuss and vote on your agenda and policies.",
+		"Updates": "Send updates to your podmates and hammer out the details."
 	}
 }
