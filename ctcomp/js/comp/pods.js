982b4fb5b077b2adf2e818ba88bb5caa635a1558
bubbleboy14 <mario.balibrera@gmail.com>
2019-11-03 19:50:29 -0800

    pods: basic resources stuff. needs: resource submission/editing/compensation

--- ctcomp/js/comp/pods.js
+++ ctcomp/js/comp/pods.js
@@ -11,9 +11,9 @@ comp.pods = {
 			main: CT.dom.div(null, "h1 mr160 relative"),
 			right: CT.dom.div(null, "h1 w160p up5 scrolly right")
 		},
-		sections: ["Info", "Updates", "Proposals", "Responsibilities",
-			"Commitments", "Services", "Requests", "Content",
-			"Products", "Codebases", "Dependencies", "Expenses"],
+		sections: ["Info", "Updates", "Resources", "Proposals",
+			"Responsibilities", "Commitments", "Services", "Requests",
+			"Content", "Products", "Codebases", "Dependencies", "Expenses"],
 		proposal: function(key) {
 			var _ = comp.pods._,
 				memship = comp.core.pod2memship(_.current.pod);
@@ -71,6 +71,14 @@ comp.pods = {
 		},
 		dependency: function(d) {
 			return CT.dom.div(d.repo, "bordered padded margined round inline-block");
+		},
+		resource: function(r) {
+			return CT.dom.div([
+				CT.dom.div(r.name, "big"),
+				r.description,
+				r.tags.join(", "),
+				"icon: " + r.icon
+			], "bordered padded margined");
 		},
 		content: function(c) {
 			return CT.dom.div([
@@ -403,6 +411,7 @@ comp.pods = {
 				unrestricted = !pod.agent && (size > 1),
 				action = unrestricted ? "show" : "hide",
 				reaction = pod.agent ? "hide" : "show",
+				resaction = (pod.variety == "resource mapping") ? "show" : "hide",
 				showSoft = (pod.variety == "software") ? "show" : "hide";
 			["Updates", "Commitments", "Services"].forEach(function(section) {
 				CT.dom[action]("tl" + section);
@@ -413,15 +422,18 @@ comp.pods = {
 			["Codebases", "Dependencies"].forEach(function(section) {
 				CT.dom[showSoft]("tl" + section);
 			});
+			CT.dom[resaction]("tlResources");
 			unrestricted || CT.dom.id("tlInfo").firstChild.onclick();
 		},
 		frame: function(data, item, plur) {
-			var _ = comp.pods._, cfg = core.config.ctcomp, n, content;
+			var _ = comp.pods._, cfg = core.config.ctcomp,
+				n, content, pcap;
 			plur = plur || item;
+			pcap = CT.parse.capitalize(plur);
 			n = _.nodes[item + "_list"] = CT.dom.div(data && data[plur].map(_[item]));
 			content = [
-				CT.dom.div(CT.parse.capitalize(plur), "biggest"),
-				cfg.blurbs[CT.parse.capitalize(plur)],
+				CT.dom.div(pcap, "biggest"),
+				cfg.blurbs[pcap],
 				n
 			];
 			data && content.unshift(CT.dom.button("new",
@@ -469,9 +481,18 @@ comp.pods = {
 				});
 			});
 		},
+		setResources: function(pod) {
+			CT.db.multi(pod.resources, function(resz) {
+				comp.pods._.frame({
+					resources: resz
+				}, "resource", "resources");
+			});
+		},
 		setDependencies: function(pod) {
 			CT.db.multi(pod.dependencies, function(deps) {
-				comp.pods._.frame({ dependencies: deps }, "dependency", "dependencies");
+				comp.pods._.frame({
+					dependencies: deps
+				}, "dependency", "dependencies");
 			});
 		},
 		setResponsibilities: function(pod) {
@@ -548,7 +569,7 @@ comp.pods = {
 				});
 			} else {
 				opts.variety = variety;
-				if (variety == "support")
+				if (variety == "support" || variety == "resource mapping")
 					return _.pod(opts);
 				comp.core.choice({
 					prompt: "how would you like to admit new members? the default mode, 'full', requires every member to approve new admissions. with the alternative, 'limited', you may designate a subset of the pod's membership to make these decisions.",
@@ -629,6 +650,7 @@ comp.pods = {
 				_.frame(data, item, item + "s");
 			});
 			_.setUpdates(pod);
+			_.setResources(pod);
 			decide.core.util.proposals(_.nodes.proposals, data.proposals);
 			_.restrictions();
 		});
