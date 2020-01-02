54f2eabf7a6525baea825c9edf96fffff87ab631
bubbleboy14 <mario.balibrera@gmail.com>
2020-01-02 15:35:42 -0800

    boards builder - preliminary init(), pod(), board() - sliderize!!

--- ctcomp/js/comp/boards.js
+++ ctcomp/js/comp/boards.js
@@ -1,7 +1,27 @@
 comp.boards = {
-	init: function() {
-		CT.dom.setContent("ctmain", [
-			CT.dom.div("message boards!", "gigantic")
+	board: function(b) {
+		return CT.dom.div([
+			CT.dom.div(b.name, "big"),
+			b.description,
+			// tags[], conversation
 		]);
+	},
+	pod: function(pod) {
+		var bnode = CT.dom.div();
+		CT.db.multi(pod.boards, function(bz) {
+			CT.dom.setContent(bnode, bz.map(comp.boards.board));
+		});
+		return CT.dom.div([
+			CT.dom.div(pod.name, "biggerest"),
+			bnode
+		]);
+	},
+	init: function() {
+		comp.core.mypods(function(pods) {
+			CT.dom.setContent("ctmain", [
+				CT.dom.div("message boards!", "gigantic"),
+				pods.map(comp.boards.pod)
+			]);
+		}, true);
 	}
 };
