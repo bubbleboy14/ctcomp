91ab0ed01ee70f4ae5cddea2be9c35226c966afe
bubbleboy14 <mario.balibrera@gmail.com>
2020-01-01 16:24:43 -0800

    Pod.boards[] (support)

--- ctcomp/model.py
+++ ctcomp/model.py
@@ -164,6 +164,7 @@ class Pod(db.TimeStampedBase):
 	resources = db.ForeignKey(kind=Resource, repeated=True)
 	dependencies = db.ForeignKey(kind="Codebase", repeated=True) # software
 	library = db.ForeignKey(kinds=[Organization, Book, Web, Media], repeated=True) # support
+	boards = db.ForeignKey(kind=Board, repeated=True) # support
 
 	def oncreate(self):
 		email_admins("New Pod", "name: %s\nvariety: %s"%(self.name, self.variety))
