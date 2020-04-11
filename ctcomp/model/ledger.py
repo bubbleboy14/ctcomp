f0dadd1bab2707b7a41935ff733819e832201e0c
bubbleboy14 <mario.balibrera@gmail.com>
2020-04-11 15:13:53 -0700

    ledger: PayBatch() (from core); Wallet.ledger() and .ledger_balance(); Audit, with ledger(), comment-specced deed(), and empty rebuild()

--- ctcomp/model/ledger.py
+++ ctcomp/model/ledger.py
@@ -1,8 +1,6 @@
-from cantools import db, config
+from cantools import db
 from cantools.util import error, log
 from ctcomp.mint import mint, balance
-
-ratios = config.ctcomp.ratios
 
 class Wallet(db.TimeStampedBase):
 	identifier = db.String() # public key
@@ -21,6 +19,18 @@ class Wallet(db.TimeStampedBase):
 		if mint(self.identifier, amount):
 			self.outstanding -= amount
 			self.put()
+
+	def ledger(self):
+		return LedgerItem.query(LedgerItem.wallet == self.key).all()
+
+	def ledger_balance(self):
+		b = 0
+		for item in self.ledger():
+			if item.polytype == "debit":
+				b -= item.amount
+			else: # deposit
+				b += item.amount
+		return b
 
 	def debit(self, amount, pod, deed, note, details=None):
 		if amount > self.outstanding:
@@ -50,3 +60,44 @@ class Deposit(LedgerItem):
 class Debit(LedgerItem):
 	pass
 
+class PayBatch(db.TimeStampedBase):
+	count = db.Integer(default=0)
+	variety = db.String()
+	details = db.Text()
+
+class Audit(db.TimeStampedBase):
+	variety = db.String(choices=["ledger", "deed", "rebuild"], default="ledger")
+	counts = db.JSON()
+	details = db.Text()
+
+	def ledger(self): # compare ledger total with recorded balance
+		log("ledger audit", important=True)
+		wallz = Wallet.query().all()
+		self.counts = {
+			"flagged": 0,
+			"wallets": len(wallz)
+		}
+		deetz = []
+		for w in wallz:
+			lb = w.ledger_balance()
+			wline = "%s: %s counted; %s recorded"%(self.key.urlsafe(),
+				lb, w.outstanding)
+			deetz.append(wline)
+			log(wline)
+			if w.outstanding != lb:
+				self.counts["flagged"] += 1
+		self.details = "\n".join(deetz)
+		self.put()
+
+	def deed(self):
+		log("deed audit", important=True)
+		# check for non-ledgerized deeds
+		# - view
+		# - verifiable
+		#   - special-case commitment.....
+		# - contribution
+		# - resource
+		# - stewardship
+
+	def rebuild(self):
+		log("rebuilding ledgers", important=True)
