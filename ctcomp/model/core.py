2dc7af1336e166317ac7084cb55f01ad7469c26e
bubbleboy14 <mario.balibrera@gmail.com>
2020-04-11 14:08:11 -0700

    extracted ledger stuff (Wallet, LedgerItem, Debit, Deposit, and a couple imports) from core

--- ctcomp/model/core.py
+++ ctcomp/model/core.py
@@ -1,63 +1,17 @@
 from datetime import datetime, timedelta
 from cantools import db, config
-from cantools.util import error, log
+from cantools.util import log
 from cantools.web import email_admins, send_mail
 from ctcoop.model import Conversation, Member, Tag, Need, Offering, Update, Task
 from ctdecide.model import Proposal
 from ctstore.model import Product
-from ctcomp.mint import mint, balance
 from compTemplates import ADJUSTMENT, ADJUSTED, INVITATION, FEEDBACK, NEED, OFFERING
 from .coders import Codebase, Contributor
+from .ledger import Wallet
 from .resources import Board, Book, Web, Media, Organization, Resource
 from .verifiables import Act, Appointment, Commitment, Delivery, Expense, Request
 
 ratios = config.ctcomp.ratios
-
-class Wallet(db.TimeStampedBase):
-	identifier = db.String() # public key
-	outstanding = db.Float(default=0)
-
-	def balance(self):
-		if not self.identifier:
-			error("your wallet is not set up")
-		return balance(self.identifier)
-
-	def mint(self, amount):
-		if not self.identifier:
-			error("your wallet is not set up")
-		if amount > self.outstanding:
-			error("you don't have that much!")
-		if mint(self.identifier, amount):
-			self.outstanding -= amount
-			self.put()
-
-	def debit(self, amount, pod, deed, note, details=None):
-		if amount > self.outstanding:
-			error("you don't have that much!")
-		Debit(wallet=self.key, pod=pod.key, deed=deed.key,
-			amount=amount, note=note, details=details).put()
-		self.outstanding -= amount
-		self.put()
-
-	def deposit(self, amount, pod, deed, note, details=None):
-		Deposit(wallet=self.key, pod=pod.key, deed=deed.key,
-			amount=amount, note=note, details=details).put()
-		self.outstanding += amount
-		self.put()
-
-class LedgerItem(db.TimeStampedBase):
-	wallet = db.ForeignKey(kind=Wallet)
-	pod = db.ForeignKey(kind="Pod")
-	deed = db.ForeignKey() # various options
-	amount = db.Float()
-	note = db.String()
-	details = db.Text()
-
-class Deposit(LedgerItem):
-	pass
-
-class Debit(LedgerItem):
-	pass
 
 class Person(Member):
 	ip = db.String()                              # optional
